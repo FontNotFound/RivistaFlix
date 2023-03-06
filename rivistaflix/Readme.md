101f51b35c99ff2458465188d383e666590dc776
CingaTuMadre <fontanaleonardomail@gmaik.com>
2023-03-06 19:17:45 +0100

    Rimosse le fregnacce di React e sistemati i comandi del package Ora l'ambiente è stabile e funzionante

--- rivistaflix/Readme.md
+++ rivistaflix/Readme.md
@@ -3,6 +3,6 @@ Git Repo - RivistaFlix
 Servizio On Demand di video relativi alle Riviste del Club delle Pagliette
 2023 -
 
-Creators: 
+Creators:
 Leonardo Fontana | Cinga Tu Madre
 Dario Danti | Riso
--- rivistaflix/src/App.js
+++ rivistaflix/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test di RivistaFlix
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/FontNotFound/RivistaFlix/tree/main/rivistaflix"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Repo del Progetto
         </a>
       </header>
     </div>
